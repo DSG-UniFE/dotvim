daac13180ac2c2371aa3abc534e1d0218f4ea6be
Mauro Tortonesi <mauro.tortonesi@unife.it>
2015-03-12 11:42:07 +0100

    Also consider system ruby when precalculating g:ruby_version_paths.

--- support/update_ruby_version_paths_rbenv.sh
+++ support/update_ruby_version_paths_rbenv.sh
@@ -5,9 +5,15 @@ RBENV_VERSION_SAVED=$RBENV_VERSION
 # redirect all output to ~/.vim_ruby_versions_paths
 exec > ~/.vim_ruby_version_paths
 
+VERSIONS=`rbenv versions --bare`
+if RBENV_VERSION=system rbenv which ruby &>/dev/null
+then
+    VERSIONS="$VERSIONS system"
+fi
+
 echo 'let g:ruby_version_paths = {'
 
-for rv in `rbenv versions --bare`
+for rv in $VERSIONS
 do
     echo -n "  \\ '$rv': "
     RBENV_VERSION=$rv # set ruby version through shell
